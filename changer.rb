9c97be30349d3f8869595b05681de4dce50665ed
ilyana19 <aries_2152@hotmail.com>
2018-09-04 18:29:04 -0400

    removed commented out code

--- changer.rb
+++ changer.rb
@@ -1,22 +1,5 @@
 class Changer
   def self.make_change(amount)
-    # coins = []
-    # until amount == 0
-    #   if amount >= 25
-    #     amount -= 25
-    #     coins << 25
-    #   elsif amount < 25 && amount >= 10
-    #     amount -= 10
-    #     coins << 10
-    #   elsif amount < 10 && amount >= 5
-    #     amount -= 5
-    #     coins << 5
-    #   else
-    #     amount -= 1
-    #     coins << 1
-    #   end
-    # end
-    # return coins
     sorted_coins = []
     coins = [25, 10, 5, 1]
       coins.each do |coin|
@@ -29,4 +12,4 @@ class Changer
   end
 end
 
-p Changer.make_change(100)
+# p Changer.make_change(100)
