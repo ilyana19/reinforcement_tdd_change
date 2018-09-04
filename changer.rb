e1e574a45ce93a9bce750e207907905f0f4e7ed6
ilyana19 <aries_2152@hotmail.com>
2018-09-04 18:28:31 -0400

    refactored code

--- changer.rb
+++ changer.rb
@@ -1,21 +1,32 @@
 class Changer
   def self.make_change(amount)
-    coins = []
-    until amount == 0
-      if amount >= 25
-        amount -= 25
-        coins << 25
-      elsif amount < 25 && amount >= 10
-        amount -= 10
-        coins << 10
-      elsif amount < 10 && amount >= 5
-        amount -= 5
-        coins << 5
-      else
-        amount -= 1
-        coins << 1
+    # coins = []
+    # until amount == 0
+    #   if amount >= 25
+    #     amount -= 25
+    #     coins << 25
+    #   elsif amount < 25 && amount >= 10
+    #     amount -= 10
+    #     coins << 10
+    #   elsif amount < 10 && amount >= 5
+    #     amount -= 5
+    #     coins << 5
+    #   else
+    #     amount -= 1
+    #     coins << 1
+    #   end
+    # end
+    # return coins
+    sorted_coins = []
+    coins = [25, 10, 5, 1]
+      coins.each do |coin|
+        while amount >= coin
+          amount -= coin
+          sorted_coins << coin
+        end
       end
-    end
-    return coins
+    return sorted_coins
   end
 end
+
+p Changer.make_change(100)
